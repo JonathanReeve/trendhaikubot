036ec5aab65bf68ab1034ef85419327c3728f2ae
Rachel Levanger <rachel.levanger@gmail.com>
2014-04-14 10:18:16 -0400

    Modified description of tweets to match results.

--- haiku.py
+++ haiku.py
@@ -13,7 +13,7 @@ for t in trends:
 
 result = api.GetSearch(term=trends[0].name, count=100)
 print "Result size is: " + str(len(result)) 
-print "Length of tweets for top trend: " 
+print "Tweets # (length) for top trend: " 
 i=1
 for r in result: 
     print "Tweet " + str(i) + " (" + str(len(r.text)) + "): " + r.text + "\n"
